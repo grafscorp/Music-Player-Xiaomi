cfe4c224289e51896b332c486e3b2530a7017ce6
Grafscorp <golyandringraf@gmail.com>
2025-01-12 01:26:17 +0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:music_player_xiaomi/components/PageLinkCards/playlists_link_card.dart';
+import 'package:music_player_xiaomi/components/PageLinkCards/recent_link_card.dart';
+import 'package:music_player_xiaomi/components/bottom_track.dart';
+import 'package:music_player_xiaomi/components/PageLinkCards/favorite_link_card.dart';
 import 'package:music_player_xiaomi/components/search_track_field.dart';
 import 'package:music_player_xiaomi/pages/drawer_page.dart';
 import 'package:music_player_xiaomi/pages/tracks_page.dart';
@@ -26,15 +30,33 @@ class _HomePageState extends State<HomePage> {
                 },
                 icon: Icon(Icons.tune_rounded),
               ),
-              SizedBox(
-                width: 15,
-              ),
               SearchTrackField(),
             ],
           ),
         ),
       ),
-      body: TracksPage(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            //Link Page
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(child: FavoriteLinkCard()),
+                Expanded(child: PlaylistsLinkCard()),
+                Expanded(child: RecentLinkCard()),
+              ],
+            ),
+
+            //Route Page
+
+            //Current Page
+            Expanded(child: TracksPage()),
+            BottomTrack()
+          ],
+        ),
+      ),
     );
   }
 }
