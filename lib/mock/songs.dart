7bbb9b8aa68fe286963cb923cd0e36f065084ab6
Grafscorp <golyandringraf@gmail.com>
2025-01-12 01:26:21 +0300

    Create songs.dart

---
+++ lib/mock/songs.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:music_player_xiaomi/components/song_list_tile.dart';
+
+final List<Widget> songList = [
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+  SongListTile(),
+];
