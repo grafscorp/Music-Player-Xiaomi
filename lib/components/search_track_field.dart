74f67e8b441a5f22b4ed1061be754bbe7be85b4b
Grafscorp <golyandringraf@gmail.com>
2025-01-11 19:30:23 +0300

    updated search track field

--- lib/components/search_track_field.dart
+++ lib/components/search_track_field.dart
@@ -11,12 +11,39 @@ class _SearchTrackFieldState extends State<SearchTrackField> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints(maxWidth: 500),
-      child: TextField(
-        decoration: InputDecoration(
-            hintText: "Поиск треков, плейлистов и артистов",
-            border: OutlineInputBorder()),
+      padding: EdgeInsets.symmetric(
+        horizontal: 8,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: IntrinsicHeight(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              constraints: BoxConstraints(maxWidth: 350),
+              child: TextField(
+                textAlign: TextAlign.start,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search_rounded),
+                  hintText: "Поиск треков, плейлистов и артистов",
+                  hintStyle: TextStyle(overflow: TextOverflow.ellipsis),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            VerticalDivider(
+              indent: 8,
+              endIndent: 8,
+            ),
+            IconButton(
+                onPressed: voiceInput, icon: Icon(Icons.mic_none_rounded))
+          ],
+        ),
       ),
     );
   }
+
+  void voiceInput() {}
 }
